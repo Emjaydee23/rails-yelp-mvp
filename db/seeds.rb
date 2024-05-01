b2e0917a4359f14868b42e84ef959612931be08b
marita jeyarajan-davidsson <atiram23@gmail.com>
2024-05-01 17:01:14 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# puts 'Creating restaurants...'
+
+# ["chinese", "italian", "japanese", "french", "belgian"].each do |restaurant_type|
+#   Restaurant.find_or_create_by!(
+#     name: restaurant_type,
+#     address: restaurant_type,
+#     phone_number: restaurant_type,
+#     category: restaurant_type
+#   )
+# end
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+  [{
+    name:'Red Lantern',
+    address:'123 High Street, London, UK',
+    phone_number: '+44 20 1234 5678',
+    category: 'Chinese'
+  },
+  {
+    name: 'Trattoria Romana',
+    address: '456 Oxford Street, Manchester, UK',
+    phone_number: '+44 161 987 6543',
+    category: 'Italian'
+  },
+  {
+    name: 'Sushi Samba',
+    address: '789 Covent Garden, London, UK',
+    phone_number: '+44 20 3456 7890',
+    category: 'Japanese'
+
+  },
+  {
+    name: 'Le Petit Bistro',
+    address: '321 Baker Street, Edinburgh, UK',
+    phone_number: '+44 131 234 5678',
+    category: 'French'
+  },
+  {
+    name: 'Belgian Waffle House',
+    address: '567 George Street, Glasgow, UK',
+    phone_number: '+44 141 876 5432',
+    category: 'Belgian'
+  }].each do |details|
+    Restaurant.create(details)
+  end
